e0c52db19825a1e8a735f1f7ec2bbd30e5277844
Abylai <abilkairablai@gmail.com>
2025-10-22 13:03:28 +0500

    Initial commit with gitignore

---
+++ DesaiVideo/DesaiVideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesaiVideoApp.swift
+//  DesaiVideo
+//
+//  Created by Abylaikhan Abilkayr on 22.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct DesaiVideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
